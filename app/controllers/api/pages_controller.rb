943510a4d87ded5438c7c49ed7aaa220f5c3f487
prestontewell <preston@tewellphoto.com>
2019-03-01 11:35:03 -0600

    add code to show params in view

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -1,5 +1,10 @@
 class Api::PagesController < ApplicationController
   def query_action
+    p "*" * 88
+    @name = params['name']
+    @job = params['job']
+    p "*" * 88
     render 'query_view.json.jbuilder'
   end
 end
+    
--- app/views/api/pages/query_view.json.jbuilder
+++ app/views/api/pages/query_view.json.jbuilder
@@ -1,1 +1,3 @@
 json.message 'hello'
+json.name @name
+json.job @job
